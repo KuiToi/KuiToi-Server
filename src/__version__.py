05d505e9afbd6e2513d908f949774f274608482b
SantaSpeen <SantaSpeen@yandex.ru>
2022-09-29 23:53:29 +0300

    Add version file

---
+++ src/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'KuiToi-Server'
+__description__ = 'BeamingDrive Multiplayer server compatible with BeamMP clients.'
+__url__ = 'https://github.com/kuitoi/kuitoi-Server'
+__version__ = '0.0.2'
+__build__ = 13
+__author__ = 'SantaSpeen'
+__author_email__ = 'SantaSpeen@gmail.com'
+__license__ = "FPA"
+__copyright__ = 'Copyright 2022 © SantaSpeen (Maxim Khomutov)'
