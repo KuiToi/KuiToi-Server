257e49027cbdaf62d6b4afe165d416eb8596e38e
SantaSpeen <SantaSpeen@gmail.com>
2023-07-01 23:27:41 +0300

    Optimize imports.

--- src/main.py
+++ src/main.py
@@ -8,22 +8,17 @@
 # Licence: FPA
 # (c) kuitoi.su 2023
 import argparse
-import asyncio
-
-from core import utils
 
 parser = argparse.ArgumentParser(description='KuiToi-Server - BeamingDrive server compatible with BeamMP clients!')
 parser.add_argument('-v', '--version', action="store_true", help='Print version and exit.', default=False)
 parser.add_argument('--config', help='Patch to config file.', nargs='?', default=None, type=str)
-log = utils.get_logger("main")
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
 
 if __name__ == '__main__':
     import core
-
     try:
         core.start()
     except KeyboardInterrupt:
         print("Exiting..")
+    except Exception as e:
+        print(e)
     core.stop()
