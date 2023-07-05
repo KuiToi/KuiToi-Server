61f92c4c08a1b1afacb456801519bd51e481af1a
SantaSpeen <SantaSpeen@gmail.com>
2023-07-05 17:15:55 +0300

    Minor

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 # Developed by KuiToi Dev
 # File main.py
 # Written by: SantaSpeen
-# Version 1.0
+# Version 1.1
 # Licence: FPA
 # (c) kuitoi.su 2023
 import argparse
@@ -12,9 +12,10 @@ import argparse
 parser = argparse.ArgumentParser(description='KuiToi-Server - BeamingDrive server compatible with BeamMP clients!')
 parser.add_argument('-v', '--version', action="store_true", help='Print version and exit.', default=False)
 parser.add_argument('--config', help='Patch to config file.', nargs='?', default=None, type=str)
-parser.add_argument('--language', help='What localisation need to use.', default="en", type=str)
+parser.add_argument('--language', help='Setting localisation.', nargs='?', default="en", type=str)
 
 if __name__ == '__main__':
+
     from core import Core
     core = Core()
     try:
